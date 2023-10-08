a2754cd29aa04b4bde0f1666de3750367b53a02d
avulic <antevulic96@gmail.com>
2023-10-08 13:52:53 +0200

    fix path to components pages

--- js/spa/app.js
+++ js/spa/app.js
@@ -5,12 +5,12 @@ import { Router } from "./router.js";
 (function () {
     function init() {
         var router = new Router([
-            new Route('games', './components/games/1_kolo.html', true),
-            new Route('1_kolo', './components/games/1_kolo.html'),
-            new Route('2_kolo', './components/games/2_kolo.html'),
-            new Route('3_kolo', './components/games/1_kolo.html'),
-            new Route('4_kolo', './components/games/1_kolo.html'),
-            new Route('5_kolo', './components/games/1_kolo.html'),
+            new Route('games', 'games/1_kolo.html', true),
+            new Route('1_kolo', 'games/1_kolo.html'),
+            new Route('2_kolo', 'games/2_kolo.html'),
+            new Route('3_kolo', 'games/1_kolo.html'),
+            new Route('4_kolo', 'games/1_kolo.html'),
+            new Route('5_kolo', 'games/1_kolo.html'),
             new Route('shop', 'shop.html'),
             new Route('about', 'about.html'),
             new Route('players', 'players.html')
