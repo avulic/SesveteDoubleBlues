7ccd79920769618caa2a80d8a3596516a72050e5
avulic <antevulic96@gmail.com>
2023-10-15 02:53:33 +0200

    fix

--- js/spa/app.js
+++ js/spa/app.js
@@ -1,20 +1,20 @@
 'use strict';
 import { Route } from "./route.js";
 import { Router } from "./router.js";
-import { Kolo1 } from '../../components/games/1_kolo.js '
-import { Players } from '../../components/players/players.js'
+import { Kolo1 } from '../components/games/1_kolo.js '
+import { Players } from '../components/players/players.js'
 
 (function () {
     function init() {
         var router = new Router([
-            new Route('1_kolo', new Kolo1(), true),
+            new Route('1_kolo', new Kolo1()),
             new Route('2_kolo', 'games/2_kolo.html'),
             new Route('3_kolo', 'games/1_kolo.html'),
             new Route('4_kolo', 'games/1_kolo.html'),
             new Route('5_kolo', 'games/1_kolo.html'),
             new Route('shop', 'shop.html'),
             new Route('about', 'about.html'),
-            new Route('players', new Players())
+            new Route('players', new Players(), true)
         ]);
     }
     init();
